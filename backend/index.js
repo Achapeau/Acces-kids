20b6a11e0c376848622c524aadce0801441227e9
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-09-06 17:11:04 +0200

    quick fix

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,4 @@
 require('dotenv').config()
 
-const app = require("./src/app.js")
-
 const port = parseInt(process.env.APP_PORT || 1337, 10)
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log(`Server is running on port ${port} 🚀`)
-  }
-})
