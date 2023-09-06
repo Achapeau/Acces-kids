51b2105a18367e7f9b71de6fdb07d4af61d63db4
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-09-06 15:35:57 +0200

    quick add

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      text: ["Telescope", "serif"],
-      heading: ["Cherry", "serif"],
+      fontFamily: {
+        text: ["Telescope", "serif"],
+        heading: ["Cherry", "serif"],
+      },
+      extend: {
+        colors: {
+          pink: "#F094C4",
+          yellow: "#F5BC49",
+          greenLight: "#B2C84F",
+          blue: "#92CCF0",
+          greenDark: "#859C39",
+          almostWhite: "#E7F2C1",
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
-
