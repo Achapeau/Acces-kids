2594d6c7f8e4af5f313402dd2db4e7fc651b11ea
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-09-07 16:30:34 +0200

    add

--- frontend/src/components/messageIA.jsx
+++ frontend/src/components/messageIA.jsx
@@ -1,34 +1,33 @@
-import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
+function messageIA({ messageOne, messageTwo, messageThree }) {
+    const [stateOne, setStateOne] = useState(false);
+    const [stateTwo, setStateTwo] = useState(false);
+    const [stateThree, setStateThree] = useState(false);
 
-const messageIA = ({ message, setMessage }) => {
-  
+    useEffect(() => {
+        setStateOne(false)
+        setStateTwo(false)
+        setStateThree(false)
+        setTimeout(() => {
+            setStateOne(true);
+        })
+        setTimeout(() => {
+            setStateTwo(true)
+        }, 4000);
+        setTimeout(() => {
+            setStateThree(true)
+        }, 6000)
+    }, [stateOne, stateTwo, stateThree])
+  return (
+    <div className='h-[70vh] w-[70vw] bg-yellow rounded-lg mx-auto text-greenDark items-center justify-center'>
+        
+        {stateOne && <div className='h-[20vh] w-[70vw] bg-pink text-3xl'>{messageOne}</div>}
+        {stateTwo && <div className='h-[20vh] w-[70vw] bg-pink text-3xl'>{messageTwo}</div>}
+        {stateThree && <div className='h-[20vh] w-[70vw] bg-pink text-3xl'>{messageThree}</div>}
+        
+    </div>
+  )
+}
 
-  const sayYes = (req) => {  
-      return req;
-  }
-
-  
-  
-  
-  const respIA = () => {
-          setTimeout(() => {
-              console.log(message.a)
-            }, 1000);
-            setTimeout(() => {
-              return message.b
-            }, 3000);
-            setTimeout(() => {
-              return message.c
-            }, 5000);
-      }
-      return (
-        <div className="h-[70vh] w-[70vW}">
-          <div className="text-almostWhite">{respIA()}</div>
-        </div>
-      )
-
-}  
-
-
-export default messageIA;
+export default messageIA
