080891781df32fce0725bad7bd7d5348ace8c7c3
Claire2311 <claire.girard@ieseg.fr>
2023-09-07 15:11:08 +0200

    small changes on footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
-function Footer () {
+function Footer() {
   return (
     <div className="bg-almostWhite h-[2rem] w-screen flex justify-center items-center absolute left-0 bottom-0">
-        <p className="text-sm text-almostBlack"> Access Kids  © 2023 By awesome Wilders™. All Rights Reserved. </p>
+      <p className="text-sm text-almostBlack">
+        Access&apos; Kids ©2023 By awesome Wilders™. All Rights Reserved.{" "}
+      </p>
     </div>
   );
 }
