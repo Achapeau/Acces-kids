865390108a14d248a5b7daaed8dcc66f5729c13b
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-09-08 12:16:33 +0200

    check

--- frontend/src/components/messageIA.jsx
+++ frontend/src/components/messageIA.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ButtonSpeakerGreen from './ButtonSpeakerGreen';
 
-function messageIA({ chiffre , alphabet, educatif }) {
+function messageIA({ chiffre , setChiffre, alphabet, setAlphabet, educatif, setEducatif }) {
     const [stateOne, setStateOne] = useState(false);
     const [stateTwo, setStateTwo] = useState(false);
     const [stateThree, setStateThree] = useState(false);
@@ -15,9 +15,6 @@ function messageIA({ chiffre , alphabet, educatif }) {
     const [value2, setValue2] = useState('')
     const [value3, setValue3] = useState('')
 
-    const [educ1, setEduc1] = useState('')
-    const [educ2, setEduc2] = useState('')
-    const [educ3, setEduc3] = useState('')
     const [verifLetter, setVerifLetter] = useState(false)
 
     useEffect(() => {
@@ -30,34 +27,36 @@ function messageIA({ chiffre , alphabet, educatif }) {
                 setStateOne(true);
                 setUrlImageOne(alphabet[0].image_animal)
                 setValue1(`${alphabet[0].corresponding_letter}, comme ${alphabet[0].name}`)
-            }, 3000)
+            }, 1000)
             setTimeout(() => {
                 setStateTwo(true)
                 setUrlImageTwo(alphabet[1].image_animal)
                 setValue2(`${alphabet[1].corresponding_letter}, comme ${alphabet[1].name}`)
-            }, 6000);
+            }, 4000);
             setTimeout(() => {
                 setStateThree(true)
                 setUrlImageThree(alphabet[2].image_animal)
                 setValue3(`${alphabet[2].corresponding_letter}, comme ${alphabet[2].name}`)
-            }, 9000)
+            }, 7000)
+            setAlphabet([])
         } else if (chiffre.length) {
             setVerifLetter(false)
             setTimeout(() => {
                 setStateOne(true);
                 setUrlImageOne(chiffre[0].image_animal)
                 setValue1(1)
-            }, 3000)
+            }, 1000)
             setTimeout(() => {
                 setStateTwo(true)
                 setUrlImageTwo(chiffre[0].image_animal)
                 setValue2(2)
-            }, 6000);
+            }, 4000);
             setTimeout(() => {
                 setStateThree(true)
                 setUrlImageThree(chiffre[0].image_animal)
                 setValue3(3)
-            }, 9000)
+            }, 7000)
+            setChiffre([])
 
         } else if (educatif) {
             setVerifLetter(false)
@@ -65,17 +64,18 @@ function messageIA({ chiffre , alphabet, educatif }) {
                 setStateOne(true)
                 setUrlImageOne('lecture.png')
                 setValue1('Sur cette page, tu peux choisir des jeux éducatifs')
-            }, 3000)
+            }, 1000)
             setTimeout(() => {
                 setStateTwo(true)   
                 setUrlImageTwo('jeuxabc.png')
                 setValue2("Apprends l'alphabet en t'amusant!")
-            }, 6000);
+            }, 4000);
             setTimeout(() => {
                 setStateThree(true)
                 setUrlImageThree('jeux123.png')
                 setValue3("Ou apprends à compter en t'amusant!")
-            }, 9000)
+            }, 7000)
+            setEducatif(false)
         }
 
     }, [alphabet, chiffre, educatif])
@@ -86,40 +86,50 @@ function messageIA({ chiffre , alphabet, educatif }) {
 
 
   return (
-    <div className='h-[70vh] w-[50vw] bg-yellow rounded-lg mx-auto text-greenDark items-center justify-center mx-20'>
+    <div className='h-[70vh] w-[50vw] bg-yellow rounded-xl mx-auto text-greenDark items-center justify-center mx-20 absolute -top-[10vh] right-[4vw]'>
         
         {stateOne && 
-        <div className='flex items-center justify-center h-[20vh] w-[50vw] text-3xl'>
+        <div className='flex items-center justify-center h-[23vh] w-[50vw] text-3xl absolute'>
            
-            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='h-[12vh] w-[12vh]' />
-            <ButtonSpeakerGreen text={`${value1}`} />
+            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='absolute left-[5vh] top-[5vw] h-[12vh] w-[12vh]' />
+            <div className='absolute right-[5vw]'>
+                <ButtonSpeakerGreen text={`${value1}`}  /> 
+            </div>
         </div>}
         {stateTwo && !educatif && !verifLetter &&
-        <div className='flex items-center justify-center h-[20vh] w-[50vw] text-3xl'>
+        <div className='flex items-center justify-center h-[23vh] w-[50vw] text-3xl'>
             
-            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='h-[12vh] w-[12vh]' />
-            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='h-[12vh] w-[12vh]' />
-            <ButtonSpeakerGreen text={`${value2}`} />
+            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='absolute left-[5vw] top-[28vh] h-[12vh] w-[12vh]' />
+            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='absolute left-[15vw] top-[28vh] h-[12vh] w-[12vh]' />
+            <div className='absolute right-[5vw] top-[32vh]'>
+                <ButtonSpeakerGreen text={`${value2}`}  /> 
+            </div>
         </div>}
         {stateTwo && (educatif || verifLetter) &&
-        <div className='flex items-center justify-center h-[20vh] w-[50vw] text-3xl'>
+        <div className='flex items-center justify-center h-[23vh] w-[50vw] text-3xl'>
            
-            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='h-[12vh] w-[12vh]' />
-            <ButtonSpeakerGreen text={`${value2}`} />
+            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='absolute left-[5vw] top-[28vh] h-[12vh] w-[12vh]' />
+            <div className='absolute right-[5vw] top-[32vh]'>
+                <ButtonSpeakerGreen text={`${value2}`}  /> 
+            </div>
         </div>}
         {stateThree && !educatif && !verifLetter &&
-        <div className='flex items-center justify-center h-[20vh] w-[50vw] text-3xl'>
+        <div className='flex items-center justify-center h-[23vh] w-[50vw] text-3xl'>
            
-            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='h-[12vh] w-[12vh]' />
-            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='h-[12vh] w-[12vh]' />
-            <img src={`${url}${urlImageThree}`} alt={`${value3}`} className='h-[12vh] w-[12vh]' />
-            <ButtonSpeakerGreen text={`${value3}`} />
+            <img src={`${url}${urlImageOne}`} alt={`${value1}`} className='absolute left-[5vw] bottom-[5vh] h-[12vh] w-[12vh]' />
+            <img src={`${url}${urlImageTwo}`} alt={`${value2}`} className='absolute left-[15vw] bottom-[5vh] h-[12vh] w-[12vh]' />
+            <img src={`${url}${urlImageThree}`} alt={`${value3}`} className='absolute left-[25vw] bottom-[5vh] h-[12vh] w-[12vh]' />
+            <div className='absolute right-[5vw] bottom-[8vh]'>
+                <ButtonSpeakerGreen text={`${value3}`}  /> 
+            </div>
         </div>}
         {stateThree  && (educatif || verifLetter) &&
-        <div className='flex items-center justify-center h-[20vh] w-[50vw] text-3xl'>
+        <div className='flex items-center justify-center h-[23vh] w-[50vw] text-3xl'>
             
-            <img src={`${url}${urlImageThree}`} alt={`${value3}`} className='h-[12vh] w-[12vh]' />
-            <ButtonSpeakerGreen text={`${value3}`} />
+            <img src={`${url}${urlImageThree}`} alt={`${value3}`} className='absolute left-[5vw] bottom-[5vh] h-[12vh] w-[12vh]' />
+            <div className='absolute right-[5vw] bottom-[8vh]'>
+                <ButtonSpeakerGreen text={`${value3}`}  /> 
+            </div>
         </div>}
 
         
