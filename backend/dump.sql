da1bd20841dbdaa0aeb72d9ed2ba1bfdb2c2c655
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-10-11 10:30:15 +0200

    first try

--- backend/dump.sql
+++ backend/dump.sql
@@ -1,106 +1,413 @@
 -- Active: 1692734908087@@127.0.0.1@3306@access_kids
+
 -- MySQL dump 10.13  Distrib 8.0.33, for macos13 (arm64)
+
 --
+
 -- Host: localhost    Database: access_kids
+
 -- ------------------------------------------------------
+
 -- Server version	8.0.33
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+/*!50503 SET NAMES utf8mb4 */
+
+;
+
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+
+;
+
+/*!40103 SET TIME_ZONE='+00:00' */
+
+;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+
+;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+
+;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+
+;
+
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+
+;
 
 --
+
 -- Table structure for table `animal`
+
 --
 
 DROP TABLE IF EXISTS `animal`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `animal` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `corresponding_letter` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `image_animal` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `image_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `sound` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+/*!50503 SET character_set_client = utf8mb4 */
+
+;
+
+CREATE TABLE
+    `animal` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `corresponding_letter` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `image_animal` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `image_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+        `sound` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 27 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
 
 --
+
 -- Dumping data for table `animal`
+
 --
 
 LOCK TABLES `animal` WRITE;
-/*!40000 ALTER TABLE `animal` DISABLE KEYS */;
-INSERT INTO `animal` VALUES (1,'Alpaga','A','alpaga.png',NULL,NULL),(2,'Éléphant','E','elephant.png',NULL,NULL),(3,'Chouette','C','chouette.png',NULL,NULL),(4,'Ours','O','ours.png',NULL,NULL),(5,'Hérisson','H','herisson.png',NULL,NULL),(6,'Iguane','I','iguane.png',NULL,NULL),(7,'Jaguar','J','jaguar.png',NULL,NULL),(8,'Koala','K','koala.png',NULL,NULL),(9,'Lion','L','lion.png',NULL,NULL),(10,'Souris','S','souris.png',NULL,NULL),(11,'Raton Laveur','R','raton-laveur.png',NULL,NULL),(12,'Yak','Y','yak.png',NULL,NULL),(13,'Vautour','V','vautour.png',NULL,NULL),(14,'Tortue','T','tortue.png',NULL,NULL),(15,'Zèbre','Z','zebre.png',NULL,NULL),(16,'Baleine','B','baleine.png',NULL,NULL),(17,'Dauphin','D','dauphin.png',NULL,NULL),(18,'Méduse','M','meduse.png',NULL,NULL),(19,'Poisson','P','poisson.png',NULL,NULL),(20,'Grenouille','G','grenouille.png',NULL,NULL),(21,'Flamant rose','F','flamant.png',NULL,NULL),(22,'Narval','N','narval.png',NULL,NULL),(23,'Quokka','Q','quokka.png',NULL,NULL),(24,'Urial','U','urial.png',NULL,NULL),(25,'Wallaby','W','wallaby.png',NULL,NULL),(26,'Xérus','X','xerus.png',NULL,NULL);
-/*!40000 ALTER TABLE `animal` ENABLE KEYS */;
+
+/*!40000 ALTER TABLE `animal` DISABLE KEYS */
+
+;
+
+INSERT INTO `animal`
+VALUES (
+        1,
+        'Alpaga',
+        'A',
+        'alpaga.png',
+        NULL,
+        NULL
+    ), (
+        2,
+        'Éléphant',
+        'E',
+        'elephant.png',
+        NULL,
+        NULL
+    ), (
+        3,
+        'Chouette',
+        'C',
+        'chouette.png',
+        NULL,
+        NULL
+    ), (
+        4,
+        'Ours',
+        'O',
+        'ours.png',
+        NULL,
+        NULL
+    ), (
+        5,
+        'Hérisson',
+        'H',
+        'herisson.png',
+        NULL,
+        NULL
+    ), (
+        6,
+        'Iguane',
+        'I',
+        'iguane.png',
+        NULL,
+        NULL
+    ), (
+        7,
+        'Jaguar',
+        'J',
+        'jaguar.png',
+        NULL,
+        NULL
+    ), (
+        8,
+        'Koala',
+        'K',
+        'koala.png',
+        NULL,
+        NULL
+    ), (
+        9,
+        'Lion',
+        'L',
+        'lion.png',
+        NULL,
+        NULL
+    ), (
+        10,
+        'Souris',
+        'S',
+        'souris.png',
+        NULL,
+        NULL
+    ), (
+        11,
+        'Raton Laveur',
+        'R',
+        'raton-laveur.png',
+        NULL,
+        NULL
+    ), (
+        12,
+        'Yak',
+        'Y',
+        'yak.png',
+        NULL,
+        NULL
+    ), (
+        13,
+        'Vautour',
+        'V',
+        'vautour.png',
+        NULL,
+        NULL
+    ), (
+        14,
+        'Tortue',
+        'T',
+        'tortue.png',
+        NULL,
+        NULL
+    ), (
+        15,
+        'Zèbre',
+        'Z',
+        'zebre.png',
+        NULL,
+        NULL
+    ), (
+        16,
+        'Baleine',
+        'B',
+        'baleine.png',
+        NULL,
+        NULL
+    ), (
+        17,
+        'Dauphin',
+        'D',
+        'dauphin.png',
+        NULL,
+        NULL
+    ), (
+        18,
+        'Méduse',
+        'M',
+        'meduse.png',
+        NULL,
+        NULL
+    ), (
+        19,
+        'Poisson',
+        'P',
+        'poisson.png',
+        NULL,
+        NULL
+    ), (
+        20,
+        'Grenouille',
+        'G',
+        'grenouille.png',
+        NULL,
+        NULL
+    ), (
+        21,
+        'Flamant rose',
+        'F',
+        'flamant.png',
+        NULL,
+        NULL
+    ), (
+        22,
+        'Narval',
+        'N',
+        'narval.png',
+        NULL,
+        NULL
+    ), (
+        23,
+        'Quokka',
+        'Q',
+        'quokka.png',
+        NULL,
+        NULL
+    ), (
+        24,
+        'Urial',
+        'U',
+        'urial.png',
+        NULL,
+        NULL
+    ), (
+        25,
+        'Wallaby',
+        'W',
+        'wallaby.png',
+        NULL,
+        NULL
+    ), (
+        26,
+        'Xérus',
+        'X',
+        'xerus.png',
+        NULL,
+        NULL
+    );
+
+/*!40000 ALTER TABLE `animal` ENABLE KEYS */
+
+;
+
 UNLOCK TABLES;
 
 --
+
 -- Table structure for table `game`
+
 --
 
 DROP TABLE IF EXISTS `game`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `game` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `coord_x` int NOT NULL,
-  `coord_y` int NOT NULL,
-  `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `is_good` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+/*!50503 SET character_set_client = utf8mb4 */
+
+;
+
+CREATE TABLE
+    `game` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `coord_x` int NOT NULL,
+        `coord_y` int NOT NULL,
+        `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `is_good` tinyint(1) NOT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
 
 --
+
 -- Dumping data for table `game`
+
 --
 
 LOCK TABLES `game` WRITE;
-/*!40000 ALTER TABLE `game` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+
+/*!40000 ALTER TABLE `game` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `game` ENABLE KEYS */
+
+;
+
 UNLOCK TABLES;
 
 --
+
 -- Table structure for table `number`
+
 --
 
 DROP TABLE IF EXISTS `number`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `number` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `sound` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+/*!50503 SET character_set_client = utf8mb4 */
+
+;
+
+CREATE TABLE
+    `number` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        `sound` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
 
 --
+
 -- Dumping data for table `number`
+
 --
 
 LOCK TABLES `number` WRITE;
-/*!40000 ALTER TABLE `number` DISABLE KEYS */;
-/*!40000 ALTER TABLE `number` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40000 ALTER TABLE `number` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `number` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
+
+;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+
+;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+
+;
+
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+
+;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+
+;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+
+;
 
 -- Dump completed on 2023-09-07 17:34:11
